57ac57f736a15e917fb5846ad79df2a0747d35e8
utsavized <utsavpandey@gmail.com>
2019-04-22 11:46:11 -0700

    PR feedback

--- src/lib/microservices/status-service/index.ts
+++ src/lib/microservices/status-service/index.ts
@@ -1,15 +1,12 @@
 import { AzureFunction, Context } from '@azure/functions';
 
 import * as statusManager from '../../common/status/status';
-import * as db from '../../common/database/database';
 import * as logger from '../../utils/logging';
 
 const moduleName = 'Status Service';
-const { database: Database } = process.env; // eslint-disable-line no-process-env
 
 const run: AzureFunction = async (context: Context, timer: any): Promise<void> => {
     try {
-        await db.connect(Database);
         await statusManager.updateStatuses();
     } catch (err) {
         logger.error(err, moduleName);
