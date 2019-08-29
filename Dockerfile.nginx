8418b2c5827eb3b31906a801ef4bb4461fadc9e2
Jesus David Garcia Gomez <jdgarcia@outlook.com>
2019-08-29 16:29:56 -0700

    Chore: Update dependencies and remove unused files

--- Dockerfile.nginx
+++
@@ -1,7 +0,0 @@
-FROM nginx:latest
-
-RUN rm -rf /etc/nginx
-
-COPY ./nginx/config /etc/nginx
-
-EXPOSE 80 443
--- scripts/images.js
+++ scripts/images.js
@@ -1,13 +1,4 @@
 module.exports = [{
-    file: 'Dockerfile.backup',
-    name: 'backup-service'
-}, {
-    file: 'Dockerfile.configmanager',
-    name: 'config-manager'
-}, {
-    file: 'Dockerfile.jobmanager',
-    name: 'job-manager'
-}, {
     file: 'Dockerfile.worker',
     name: 'worker-service'
 }];
